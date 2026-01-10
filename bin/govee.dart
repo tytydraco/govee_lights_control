117d8f82ba17eecc21c08dacf40fa80fd6182db2
Tyler Nijmeh <tylernij@gmail.com>
2026-01-09 19:17:34 -0600

    Add API osenv gitignore
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- bin/govee.dart
+++ bin/govee.dart
@@ -1,9 +1,16 @@
 import 'dart:async';
+import 'dart:io';
 
+import 'package:govee_lights_control/api.dart';
 import 'package:govee_lights_control/devices/h607c_floor_lamp_2.dart';
 import 'package:govee_lights_control/protocol/skylight.dart';
 
 Future<void> main(List<String> arguments) async {
+  if (apiKey == null) {
+    stdout.writeln('GOVEE_API_KEY is not set');
+    exit(1);
+  }
+
   final now = DateTime.now();
   final sunrise = DateTime(now.year, now.month, now.day, 6);
   final sunset = DateTime(now.year, now.month, now.day, 18);
